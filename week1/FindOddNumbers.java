a8e152b72bdb18c087a89a564eefc2b75bbff38b
Gayathirirajarathnam <gayathirirks@gmail.com>
2025-08-07 17:50:28 +0530

    Add files via upload

---
+++ week1/FindOddNumbers.java
@@ -0,0 +1,15 @@
+package Week1.Day2;
+
+public class FindOddNumbers {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			for (int maxRange=1;maxRange<=10;maxRange++)
+			{
+				if(maxRange%2!=0)
+					System.out.println(maxRange);
+			}
+				}
+
+			}
