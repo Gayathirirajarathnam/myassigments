373d83253dbfc1642f82237c1acaeb6d272023ea
Gayathirirajarathnam <gayathirirks@gmail.com>
2025-08-07 17:28:46 +0530

    Add files via upload

---
+++ week1/Chrome.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float version = 91.0F;
+String Developer="Google";
+Boolean isBeta = false;
+int Releaseyear=2008;
+char shortcutkey='C';
+System.out.println(isBeta);
+System.out.println(version);
+System.out.println(Developer);
+System.out.println(Releaseyear);
+System.out.println(version);
+System.out.println(shortcutkey);
+	}
+}
